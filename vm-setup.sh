0eb51309da0e09d2f6765fd27f95e3bd500501c0
m3tra <m3tra1337@gmail.com>
2023-03-30 23:33:22 +0100

    Update vm-setup.sh

--- vm-setup.sh
+++ vm-setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+WHITE="\033[0m"
+RED="\033[38;5;1m"
+GREEN="\033[38;5;47m"
+BLUE="\033[38;5;14m"
+YELLOW="\033[33m"
+PURPLE="\033[38;5;13m"
+
 if [[ $EUID -ne 0 ]]; then
 	echo "$0 requires sudo."
 	exit 2
 fi
-
-apt update && apt upgrade -y
 
 
 
@@ -17,7 +22,6 @@ read -p "Choose user: " USER_NAME
 
 USER_EXISTS=$(cat /etc/passwd | grep $USER_NAME | wc -l)
 if [[ USER_EXISTS -eq 0 ]]; then
-# if [[ $USER != $USER_NAME ]]; then
 	adduser $USER_NAME
 	su $USER_NAME
 fi
@@ -28,51 +32,87 @@ fi
 # Docker #
 ##########
 
+echo
+echo $GREEN"Setting up Docker..."$WHITE
+
+echo -n "  Updating system"
+
+apt update && \
+apt upgrade -y 1>/dev/null
+
 # Install dependencies
+echo -n "  Installing dependencies"
+
 apt install \
 	ca-certificates \
 	curl \
 	gnupg \
-	-y
+	-y \
+	1>/dev/null
 
+echo $GREEN" DONE"$WHITE
+
+
+echo -n "  Adding gpg key and docker repo to apt"
 
 # Add gpg key
-
 mkdir -m 0755 -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-
 # Add repository to apt
-
 echo \
 	"deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
 	"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
 	tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-apt update
-apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+echo $GREEN" DONE"$WHITE
+
+
+# Install docker
+echo -n "  Installing docker"
+apt update && \
+apt install \
+	docker-ce \
+	docker-ce-cli \
+	containerd.io \
+	docker-buildx-plugin \
+	docker-compose-plugin \
+	-y \
+	1>/dev/null
+
+echo $GREEN" DONE"$WHITE
 
 
 # Enable docker service and test installation
-
+echo -n "  Enabling docker service"
 systemctl enable docker.service
 systemctl enable containerd.service
+echo $GREEN" DONE"$WHITE
 
+echo -n "  Starting docker service"
 systemctl start docker.service
 systemctl start containerd.service
+echo $GREEN" DONE"$WHITE
 
+echo -n "  Tesing hello-world container"
 docker run hello-world
+echo $GREEN" DONE"$WHITE
 
 
 # Make sure non-root user doesn't require sudo to use docker
-groupadd docker
+# GROUP_EXISTS=$(groups | grep docker | wc -l)
+# if [[ GROUP_EXISTS -eq 0 ]]; then
+# 	adduser $USER_NAME
+# 	su $USER_NAME
+# fi
+groupadd docker 2>/dev/null
 usermod -aG docker $USER_NAME
-su
-exit
-# su $USER_NAME
+# su
+# exit
+su $USER_NAME
 
-chown $USER_NAME:$USER_NAME /home/$USER_NAME/.docker -R
-chmod g+rwx $HOME/.docker -R
+chown -R $USER_NAME:$USER_NAME /home/$USER_NAME/.docker
+chmod -R g+rwx $HOME/.docker
 
 
 
@@ -80,17 +120,22 @@ chmod g+rwx $HOME/.docker -R
 # Utils #
 #########
 
+echo
+echo -n $GREEN"Setting up utils (sudo, nano, htop)..."$WHITE
+
 # Install nice-to-haves
 apt install \
 	sudo \
 	nano \
 	htop \
-	-y
+	-y \
+	1>/dev/null
 
 
 # Grant user sudo
-
 echo "$USER ALL=(ALL:ALL) ALL" > /etc/sudoers
+
+echo $GREEN" DONE"$WHITE
 
 
 
@@ -98,15 +143,27 @@ echo "$USER ALL=(ALL:ALL) ALL" > /etc/sudoers
 # Firewall #
 ############
 
-apt install ufw
+echo
+echo $GREEN"Setting up UFW..."$WHITE
 
+echo -n "  Installing"
+apt install ufw -y 1>/dev/null
+echo -n $GREEN" DONE"$WHITE
+
+echo -n "  Enabling"
 systemctl enable ufw.service
 systemctl start ufw.service
-
 ufw enable
+echo -n $GREEN" DONE"$WHITE
 
+echo -n "  Adding rules"
 ufw allow 80,443,8080,9443/tcp
+echo -n $GREEN" DONE"$WHITE
 
+echo -n "  Reloading"
 ufw reload
+echo $GREEN" DONE"$WHITE
+
+echo $GREEN"DONE"$WHITE
 
 make
