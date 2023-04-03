2df3309d6dd7aba6fe1744f0f8e02d914227a33b
m3tra <m3tra1337@gmail.com>
2023-04-03 04:54:34 +0100

    Update vm-setup.sh

--- vm-setup.sh
+++ vm-setup.sh
@@ -47,10 +47,10 @@ printf "${GREEN} DONE\n${WHITE}"
 echo -n "    + Installing dependencies"
 
 apt-get install \
-	ca-certificates \
-	curl \
-	gnupg \
-	-y 1>/dev/null
+		ca-certificates \
+		curl \
+		gnupg \
+		-y 1>/dev/null
 
 printf "${GREEN} DONE\n${WHITE}"
 
@@ -80,13 +80,13 @@ echo -n "    + Installing docker"
 
 apt-get update 1>/dev/null
 apt-get install \
-	docker-ce \
-	docker-ce-cli \
-	containerd.io \
-	docker-buildx-plugin \
-	docker-compose \
-	docker-compose-plugin \
-	-y 1>/dev/null
+		docker-ce \
+		docker-ce-cli \
+		containerd.io \
+		docker-buildx-plugin \
+		docker-compose \
+		docker-compose-plugin \
+		-y 1>/dev/null
 
 printf "${GREEN} DONE\n${WHITE}"
 
@@ -118,17 +118,21 @@ printf "${GREEN}DONE\n${WHITE}"
 
 
 # Make sure non-root user doesn't require sudo to use docker
+echo -n "    + Adding $USER_NAME to docker group"
+
 # GROUP_EXISTS=$(groups | grep docker | wc -l)
 # if [[ GROUP_EXISTS -eq 0 ]]; then
 # 	adduser $USER_NAME
 # 	su $USER_NAME
 # fi
 groupadd docker 2>/dev/null
-/usr/sbin/usermod -aG docker $USER_NAME
+usermod -aG docker $USER_NAME
 
 mkdir -m 770 -p /home/$USER_NAME/.docker
 chown -R $USER_NAME:$USER_NAME /home/$USER_NAME/.docker
 # chmod -R g+rwx /home/$USER_NAME/.docker
+
+printf "${GREEN} DONE\n${WHITE}"
 
 
 
@@ -140,18 +144,15 @@ printf "${GREEN}\nSetting up utils (sudo, nano, htop)...${WHITE}"
 
 # Install nice-to-haves
 apt-get install \
-	zsh \
-	sudo \
-	make \
-	nano \
-	htop \
-	-y 1>/dev/null
-
-chsh -s /bin/zsh $USER_NAME
+		sudo \
+		make \
+		nano \
+		htop \
+		-y 1>/dev/null
 
 
 # Grant user sudo
-echo "$USER_NAME ALL=(ALL:ALL) ALL" > /etc/sudoers
+echo "$USER_NAME ALL=(ALL:ALL) ALL" >> /etc/sudoers
 
 printf "${GREEN} DONE\n${WHITE}"
 
