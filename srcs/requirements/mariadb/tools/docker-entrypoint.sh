5c31943ad5c4ae59b2369be37342856a8f0b3fcb
m3tra <m3tra1337@gmail.com>
2023-04-06 01:08:15 +0100

    Update docker-entrypoint.sh

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -120,7 +120,7 @@
 
 service mysql start
 
-mysql -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE};"
-mysql -u root -e "USE '${MYSQL_DATABASE}'; GRANT ALL PRIVILEGES ON * TO '${MYSQL_USER}'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-mysql -u root -e "alter user 'root'@'localhost' identified by '${MYSQL_ROOT_PASSWORD}'";
+mysql -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mysql -u root -e "CREATE DATABASE $MYSQL_DATABASE;"
+mysql -u root -e "USE '$MYSQL_DATABASE'; GRANT ALL PRIVILEGES ON * TO '$MYSQL_USER'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
+mysql -u root -e "alter user 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD'";
