22fb7e2753f0c7b275f89761b7a386b948782432
m3tra <m3tra1337@gmail.com>
2023-08-31 07:20:17 +0100

    wp-cli test

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -34,58 +34,58 @@ set -e
 
 # mkdir -p /var/www/html
 
-if [ ! -d "/var/www/html/wordpress" ]; then
-	echo "no folder"
-	wget https://wordpress.org/latest.tar.gz
-	tar -xzvf latest.tar.gz
-	rm -r latest.tar.gz
-fi
+# if [ ! -d "wordpress" ]; then
+# 	echo "no folder"
+# 	wget https://wordpress.org/latest.tar.gz
+# 	tar -xzvf latest.tar.gz
+# 	rm -r latest.tar.gz
+# fi
 
-cd /var/www/html/wordpress
+# cd /var/www/html/wordpress
 
-if [ ! -f /var/www/html/wordpress/index.php ]; then
-	wp core download --allow-root
-fi
+# if [ ! -f /var/www/html/wordpress/index.php ]; then
+# 	wp core download --allow-root
+# fi
 
-if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
-	wp config create \
-		--dbname=$MYSQL_DATABASE \
-		--dbuser=$MYSQL_USER \
-		--dbpass=$MYSQL_ROOT_PASSWORD \
-		--dbhost=mariadb \
-		--allow-root
-	# wp config set WP_REDIS_HOST redis --allow-root
-	# wp config set WP_REDIS_PORT 6379 --raw --allow-root
-	# wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
-	# wp config set WP_CACHE true --raw --allow-root
+# if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
+# 	wp config create \
+# 		--dbname=$MYSQL_DATABASE \
+# 		--dbuser=$MYSQL_USER \
+# 		--dbpass=$MYSQL_ROOT_PASSWORD \
+# 		--dbhost=mariadb \
+# 		--allow-root
+# 	# wp config set WP_REDIS_HOST redis --allow-root
+# 	# wp config set WP_REDIS_PORT 6379 --raw --allow-root
+# 	# wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
+# 	# wp config set WP_CACHE true --raw --allow-root
 
-	wp core install \
-		--url=$DOMAIN_NAME \
-		--title="$WP_TITLE" \
-		--admin_user=$WP_ADMIN_USER \
-		--admin_password=$WP_ADMIN_PASSWORD \
-		--admin_email=$WP_ADMIN_EMAIL \
-		--skip-email \
-		--allow-root
+# 	wp core install \
+# 		--url=$DOMAIN_NAME \
+# 		--title="$WP_TITLE" \
+# 		--admin_user=$MYSQL_USER \
+# 		--admin_password=$MYSQL_USER_PASSWORD \
+# 		--admin_email=$WP_ADMIN_EMAIL \
+# 		--skip-email \
+# 		--allow-root
 
-	wp user create \
-		$WP_USER \
-		$WP_USER_EMAIL \
-		--user_pass=$WP_USER_PASSWORD \
-		--role=author \
-		--allow-root
+# 	wp user create \
+# 		$WP_USER \
+# 		$WP_USER_EMAIL \
+# 		--user_pass=$WP_USER_PASSWORD \
+# 		--role=author \
+# 		--allow-root
 
-	# wp plugin install redis-cache --activate --allow-root
-	# wp plugin update --all --allow-root
-	# wp redis enable --allow-root
+# 	# wp plugin install redis-cache --activate --allow-root
+# 	# wp plugin update --all --allow-root
+# 	# wp redis enable --allow-root
 
-	chown -R www-data:www-data /var/www/html/wordpress
-fi
+# 	chown -R www-data:www-data /var/www/html/wordpress
+# fi
 
 # create the PID file(/run/php/php7.4-fpm.pid)
-service php7.4-fpm start
-service php7.4-fpm stop
+# service php7.4-fpm start
+# service php7.4-fpm stop
 
-echo "*****Starting Wordpress Container*****"
+# echo "*****Starting Wordpress Container*****"
 
 exec "$@"
