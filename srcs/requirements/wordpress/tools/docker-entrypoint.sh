09eedc30be8b21b24523cc39ee08315f61f569bf
m3tra <m3tra1337@gmail.com>
2023-08-31 07:22:51 +0100

    q

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -88,4 +88,39 @@ set -e
 
 # echo "*****Starting Wordpress Container*****"
 
+
+
+
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
+chmod +x wp-cli.phar \
+mv wp-cli.phar /usr/local/bin/wp \
+wp core download \
+	--allow-root \
+wp config create \
+	--dbname=$MYSQL_DATABASE \
+	--dbuser=$MYSQL_USER \
+	--dbpass=$MYSQL_ROOT_PASSWORD \
+	--dbhost=$MYSQL_HOSTNAME \
+	--allow-root \
+wp db create \
+	--allow-root\
+wp core install \
+	--url=$DOMAIN_NAME \
+	--title="$WP_TITLE" \
+	--admin_user=$MYSQL_USER \
+	--admin_password=$MYSQL_USER_PASSWORD \
+	--admin_email=$WP_ADMIN_EMAIL \
+	--skip-email \
+	--allow-root \
+wp core update \
+	--allow-root \
+wp plugin update --all \
+	--allow-root \
+wp user create \
+	$WP_USER \
+	$WP_USER_EMAIL \
+	--user_pass=$WP_USER_PASSWORD \
+	--role=author \
+	--allow-root
+
 exec "$@"
