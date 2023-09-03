cd5eace10fc0b378756b4cc410d55d3aedf474c4
m3tra <m3tra1337@gmail.com>
2023-09-03 01:55:39 +0100

    no entrypoint

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -17,7 +17,7 @@
 
 if [ ! -d "/var/lib/mysql/$MYSQL_DB_NAME" ]; then
 
-mysql_install_db --user=$MYSQL_USER
+mysql_install_db --user=mysql
 service mariadb start
 
 # mysql_secure_installation << EOF
@@ -31,14 +31,14 @@ service mariadb start
 # y
 # EOF
 
-echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASS'; FLUSH PRIVILEGES;" | mysql -uroot
+# echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASS'; FLUSH PRIVILEGES;" | mysql -uroot
 
-echo "\
-CREATE DATABASE IF NOT EXISTS $MYSQL_DB_NAME; \
-GRANT ALL ON $MYSQL_DB_NAME.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; \
-FLUSH PRIVILEGES;" | mariadb -u root
+# echo "\
+# CREATE DATABASE IF NOT EXISTS $MYSQL_DB_NAME; \
+# GRANT ALL ON $MYSQL_DB_NAME.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; \
+# FLUSH PRIVILEGES;" | mariadb -u root
 
-mariadb -u root -p $MYSQL_ROOT_PASS $MYSQL_DB_NAME < wordpress2.sql
+# mariadb -u root -p $MYSQL_ROOT_PASS $MYSQL_DB_NAME < wordpress2.sql
 
 fi
 
