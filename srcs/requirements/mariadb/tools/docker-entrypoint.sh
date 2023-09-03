2ebb3c6c73ca6921e25cd8672805952d48638d72
m3tra <m3tra1337@gmail.com>
2023-09-03 03:36:36 +0100

    pls3546364

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,50 +1,37 @@
 #!/bin/sh
 
-# DATABASE_PATH=/var/lib/mysqld/$MYSQL_DB_NAME
+mysql_install_db
+/etc/init.d/mysql start
 
-# if [ ! -d "$DATABASE_PATH" ]; then
-# 	service mysql start;
-# 	mysql -u root --execute= \
-# 		"CREATE DATABASE IF NOT EXISTS $MYSQL_DB_NAME; \
-# 		GRANT ALL ON $MYSQL_DB_NAME.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION;; \
-# 		ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASS'; \
-# 		FLUSH PRIVILEGES;"
-# 	# mysql -uroot -p$MYSQL_ROOT_PASS $MYSQL_DB_NAME < db-config.sql ;
-# 	mysqladmin -uroot -p$MYSQL_ROOT_PASS shutdown;
-# fi
-
-# mysql_install_db
+#Check if the database exists
 
 if [ ! -d "/var/lib/mysql/$MYSQL_DB_NAME" ]; then
 
-mysql_install_db
-service mariadb start
+# Set root option so that connexion without root password is not possible
+mysql_secure_installation << _EOF_
 
-mysql_secure_installation << EOF
-
-y
-$MYSQL_ROOT_PASS
-$MYSQL_ROOT_PASS
-y
+Y
+root4life
+root4life
+Y
 n
-y
-y
-EOF
+Y
+Y
+_EOF_
 
-mkdir -p /var/lib/mysql
-chmod -R 777 /var/lib/mysql
-chown -R mysql:mysql /var/lib/mysql
+#Add a root user on 127.0.0.1 to allow remote connexion
+#Flush privileges allow to your sql tables to be updated automatically when you modify it
+#mysql -uroot launch mysql command line client
+echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASS'; FLUSH PRIVILEGES;" | mysql -uroot
 
-mkdir -p /run/mysqld
-chmod -R 777 /run/mysqld
-chown -R mysql:mysql /run/mysqld
+#Create database and user in the database for wordpress
+echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DB_NAME; GRANT ALL ON $MYSQL_DB_NAME.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASS'; FLUSH PRIVILEGES;" | mysql -u root
 
-mariadb -u mysql -p $MYSQL_ROOT_PASS $MYSQL_DB_NAME < db-config.sql
+#Import database in the mysql command line
+mysql -uroot -p$MYSQL_ROOT_PASS $MYSQL_DB_NAME < db-config.sql
 
 fi
 
-# /etc/init.d/mysql stop
-service mariadb stop
+/etc/init.d/mysql stop
 
 exec "$@"
-# mysqld --user=mysql --init-file=/db-config.sql
