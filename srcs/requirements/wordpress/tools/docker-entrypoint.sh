9a654957ac178264c494cc7e37a7405c88829ed9
m3tra <m3tra1337@gmail.com>
2023-09-03 10:11:48 +0100

    removed unneeded stuff

--- srcs/requirements/wordpress/tools/docker-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-entrypoint.sh
@@ -1,95 +1,5 @@
 #!/bin/bash
 
-# wp core download --allow-root
-
-# wp config create --dbname=$MYSQL_DB_NAME --dbuser=$MYSQL_USER --dbpass=$MYSQL_ROOT_PASS --dbhost=mariadb --allow-root
-
-# # wp config set WP_REDIS_HOST redis --allow-root
-# # wp config set WP_REDIS_PORT 6379 --raw --allow-root
-# # wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
-# # wp config set WP_CACHE true --raw --allow-root
-
-# wp core install --url=$DOMAIN_NAME --title="$WP_TITLE" --admin_user=$MYSQL_USER --admin_password=$MYSQL_ROOT_PASS --admin_email=$WORDPRESS_USER_EMAIL --skip-email --allow-root
-# wp user create $MYSQL_USER $WORDPRESS_USER_EMAIL --user_pass=$MYSQL_PASSWORD --role=author --allow-root
-
-# # wp plugin install redis-cache --activate --allow-root
-# # wp plugin update --all --allow-root
-# # wp redis enable --allow-root
-
-# chown -R www-data:www-data /var/www/html/wordpress
-
-# set -e
-
-# # waiting for mariadb
-# while ! mariadb -hmariadb -u$MYSQL_USER -p$MYSQL_ROOT_PASS $MYSQL_DB_NAME > /dev/null 2>&1; do
-# 	echo "Waiting for MariaDB ..."
-# 	sleep 2
-# done
-
-# # waiting for redis
-# while ! redis-cli -h redis > /dev/null 2>&1; do
-# 	echo "Waiting for Redis ..."
-# 	sleep 2
-# done
-
-# mkdir -p /var/www/html
-
-# if [ ! -d "wordpress" ]; then
-# 	echo "no folder"
-# 	wget https://wordpress.org/latest.tar.gz
-# 	tar -xzvf latest.tar.gz
-# 	rm -r latest.tar.gz
-# fi
-
-# cd /var/www/html/wordpress
-
-# if [ ! -f /var/www/html/wordpress/index.php ]; then
-# 	wp core download --allow-root
-# fi
-
-# if [ ! -f /var/www/html/wordpress/wp-config.php ]; then
-# 	wp config create \
-# 		--dbname=$MYSQL_DB_NAME \
-# 		--dbuser=$MYSQL_USER \
-# 		--dbpass=$MYSQL_ROOT_PASS \
-# 		--dbhost=mariadb \
-# 		--allow-root
-# 	# wp config set WP_REDIS_HOST redis --allow-root
-# 	# wp config set WP_REDIS_PORT 6379 --raw --allow-root
-# 	# wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
-# 	# wp config set WP_CACHE true --raw --allow-root
-
-# 	wp core install \
-# 		--url=$DOMAIN_NAME \
-# 		--title="$WP_TITLE" \
-# 		--admin_user=$MYSQL_USER \
-# 		--admin_password=$MYSQL_PASS \
-# 		--admin_email=$WP_ADMIN_EMAIL \
-# 		--skip-email \
-# 		--allow-root
-
-# 	wp user create \
-# 		$WP_USER \
-# 		$WP_USER_EMAIL \
-# 		--user_pass=$WP_USER_PASSWORD \
-# 		--role=author \
-# 		--allow-root
-
-# 	# wp plugin install redis-cache --activate --allow-root
-# 	# wp plugin update --all --allow-root
-# 	# wp redis enable --allow-root
-
-# 	chown -R www-data:www-data /var/www/html/wordpress
-# fi
-
-# create the PID file(/run/php/php7.4-fpm.pid)
-# service php7.4-fpm start
-# service php7.4-fpm stop
-
-# echo "*****Starting Wordpress Container*****"
-
-
-
 if [ ! -f /var/www/html/wp-config-sample.php ]; then
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
@@ -99,7 +9,6 @@ wp.phar core download \
 	--path=/var/www/html \
 	--allow-root
 
-if [ ! -f /var/www/html/wp-config.php ]; then
 wp.phar config create \
 	--path=/var/www/html \
 	--dbname=$MYSQL_DB_NAME \
@@ -107,13 +16,10 @@ wp.phar config create \
 	--dbpass=$MYSQL_PASS \
 	--dbhost=$MYSQL_HOSTNAME \
 	--allow-root
-fi
 
 wp.phar db create \
 	--path=/var/www/html \
 	--allow-root
-# 	--allow-root \
-# || true
 
 wp.phar core install \
 	--path=/var/www/html \
