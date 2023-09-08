5b9b00e8cab2272af8cdc21d70e0f2a5e95701cb
m3tra <m3tra1337@gmail.com>
2023-09-08 17:18:42 +0100

    anda la pa

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 mysql_install_db --user=mysql --datadir=/var/lib/mysql
-# service mariadb start
+service mariadb start
 
-# if [ ! -d "/var/lib/mysql/$MYSQL_DB_NAME" ]; then
-# mysql_secure_installation << EOF
-# $MYSQL_ROOT_PASS
-# y
-# n
-# y
-# y
-# y
-# y
-# EOF
-# fi
+if [ ! -d "/var/lib/mysql/$MYSQL_DB_NAME" ]; then
+mysql_secure_installation << EOF
+$MYSQL_ROOT_PASS
+y
+n
+y
+y
+y
+y
+EOF
+fi
 
-# service mariadb stop
+service mariadb stop
 
 mysqld --user=mysql --init-file=/db-config.sql --bind-address=0.0.0.0
 # exec "$@"
